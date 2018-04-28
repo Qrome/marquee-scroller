85ea5f061e8e43e09415b9b6ef1304e51cace1f7
Qrome <32021227+Qrome@users.noreply.github.com>
2018-04-27 20:55:48 -0700

    removed duplicate include

--- marquee/Settings.h
+++ marquee/Settings.h
@@ -31,7 +31,6 @@ SOFTWARE.
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
 #include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
 #include <WiFiManager.h>
 #include <ESP8266mDNS.h>
 #include <ArduinoOTA.h>
